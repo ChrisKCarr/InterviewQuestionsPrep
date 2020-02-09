44def230e03ec81980cb0cc6b252589781283ffe
Chris Carr <krzysztof@Chriss-MacBook-Pro.local>
2020-02-09 17:14:10 +0100

    Hash Table Interview Questions

---
+++ FirstRecurringCharacter.js
@@ -0,0 +1,23 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+function firstRecurringCharacter(input) {
+  var s = new Set();
+  return input.find(v => s.has(v) || !s.add(v));
+}
+
+//Bonus... What if we had this:
+// [2,5,5,2,3,5,1,2,4]
+// return 5 because the pairs are before 2,2
+
+console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter([2, 1, 1, 2, 3, 5, 1, 2, 4]));
+console.log(firstRecurringCharacter([2, 3, 4, 5]));
+console.log(firstRecurringCharacter([2, 5, 5, 2, 3, 5, 1, 2, 4]));

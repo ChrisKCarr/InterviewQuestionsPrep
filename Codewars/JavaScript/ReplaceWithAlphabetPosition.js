f680f8fa4fec4523030ce8320fa543ec67a97610
Chris Carr <chriskcarrdev@gmail.com>
2021-01-29 17:43:45 +0100

    Leetcode/Codewars practise

--- Codewars/JavaScript/ReplaceWithAlphabetPosition.js
+++ Codewars/JavaScript/ReplaceWithAlphabetPosition.js
@@ -7,7 +7,11 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
 function alphabetPosition(text) {
-  const splitText = text.split("");
+  var result = "";
+  for (var i = 0; i < text.length; i++) {
+    var code = text.toUpperCase().charCodeAt(i);
+    if (code > 64 && code < 91) result += code - 64 + " ";
+  }
 
-  return text;
+  return result.slice(0, result.length - 1);
 }
